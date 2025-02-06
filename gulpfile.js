8c33ec2be37ccfec043af5291bdf60c1859f0b5e
MichelPianaro <teconhecer27@gmail.com>
2025-02-06 15:28:53 -0300

    segundo commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+    return gulp.src('./src/images/**/*.{jpg,jpeg,png,gif,svg}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'));
 }
 
 gulp.task('watch', function () {
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('src/styles/**/*.scss', gulp.series(styles));
+    gulp.watch('src/images/**/*.{jpg,jpeg,png,gif,svg}', gulp.series(images));
+    gulp.watch('src/scripts/**/*.js', gulp.series(scripts));
 });
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*.{jpg,jpeg,png,gif,svg}', gulp.parallel(images));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
+});
+
+const hero = document.querySelector(".hero");
+const left = document.querySelector(".split.left");
+const right = document.querySelector(".split.right");
+const overlay = document.querySelector(".overlay");
+
+hero.addEventListener("mousemove", (e) => {
+    let percentage = (e.clientX / window.innerWidth) * 100;
+    left.style.width = `${percentage}%`;
+    right.style.width = `${100 - percentage}%`;
+    overlay.style.left = `${percentage}%`;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tab-link");
+    const tabPanes = document.querySelectorAll(".tab-pane");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(t => t.classList.remove("active"));
+            tabPanes.forEach(p => p.classList.remove("active"));
+
+            tab.classList.add("active");
+            document.getElementById(tab.dataset.tab).classList.add("active");
+        });
+    });
+});
